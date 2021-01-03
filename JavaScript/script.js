70888bb64c1c44ae95374140884177bddb86d12d
HelyeFab <emmanuelfabiani23@gmail.com>
2021-01-03 16:18:34 +0000

    changed from api to json

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,28 +14,35 @@ const clickedSigns = document.querySelectorAll(".sign");
 const horoscopeTitle = document.querySelector(".horoscopePanelBox-title");
 const horoscopeContent = document.querySelector(".horoscopePanelBox-content");
 
-
-
-
 // Functions
 
-
-
 //  EventListeners
+// clickedSigns.forEach((el) => {
+//   el.addEventListener("click", () => {
+//     let clickedSignName = el.innerText.toLowerCase(); //gives the name of sign the user clicks on
+//     horoscopeTitle.innerText = clickedSignName.toUpperCase();
+//     fetch(
+//       `http://sandipbgt.com/theastrologer/api/horoscope/${clickedSignName}/today`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+
+//   });
+// });
+
 clickedSigns.forEach((el) => {
   el.addEventListener("click", () => {
-    let clickedSignName = el.innerText.toLowerCase(); //gives the name of sign the user clicks on
+    let clickedSignName = el.innerText; //gives the name of sign the user clicks on
     horoscopeTitle.innerText = clickedSignName.toUpperCase();
-    fetch(
-      `/http://sandipbgt.com/theastrologer/api/horoscope/${clickedSignName}/today`
-    )
+    fetch("../horoscope.json")
       .then((response) => response.json())
-      .then((data) => (horoscopeContent.innerText = data.horoscope));
-    
-    
-    
-      
-      
+      .then((data) => {
+        const horoscopes = data.horoscopes;
+        for (let i = 0; i < horoscopes.length; i++) {
+          if (horoscopes[i].sunsign === clickedSignName) {
+            horoscopeContent.innerText = horoscopes[i].horoscope;
+          }
+        }
+      });
   });
 });
-

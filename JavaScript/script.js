be0e6ebdb6c5ca21ed77293a873bd4395725b582
HelyeFab <emmanuelfabiani23@gmail.com>
2021-01-03 12:25:13 +0000

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
+  });
+});
+
+//! Horoscope Logic
+const clickedSigns = document.querySelectorAll(".sign");
+const horoscopeTitle = document.querySelector(".horoscopePanelBox-title");
+const horoscopeContent = document.querySelector(".horoscopePanelBox-content");
+
+
+
+
+// Functions
+
+
+
+//  EventListeners
+clickedSigns.forEach((el) => {
+  el.addEventListener("click", () => {
+    let clickedSignName = el.innerText.toLowerCase(); //gives the name of sign the user clicks on
+    horoscopeTitle.innerText = clickedSignName.toUpperCase();
+    fetch(
+      `http://sandipbgt.com/theastrologer/api/horoscope/${clickedSignName}/today`)
+      .then((response) => response.json())
+      .then((data) =>
+        
+       horoscopeContent.innerText = data.horoscope);
+    
+    
+    
+      
+      
+  });
+});
+
